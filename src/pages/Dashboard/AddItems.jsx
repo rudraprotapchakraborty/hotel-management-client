6cf3c7b8585940244c75355ef0d91d4e2d700672
Rudra <rudra.phymos@gmail.com>
2025-01-26 17:47:58 +0530

    add items page added

--- src/pages/Dashboard/AddItems.jsx
+++ src/pages/Dashboard/AddItems.jsx
@@ -2,14 +2,17 @@ import { useForm } from "react-hook-form";
 import SectionTitle from "../../components/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddItems = () => {
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, reset } = useForm();
     const axiosPublic = useAxiosPublic();
+    const axiosSecure = useAxiosSecure();
     const onSubmit = async (data) => {
         const imageFile = {image: data.image[0]};
         const res = await axiosPublic.post(image_hosting_api, imageFile, {
@@ -17,7 +20,25 @@ const AddItems = () => {
                 'Content-Type': 'multipart/form-data'
             }
         });
-        console.log(res.data.data.url);
+        if (res.data.success) {
+            const mealItem = {
+                name: data.name,
+                category: data.category,
+                price: parseFloat(data.price),
+                recipe: data.recipe,
+                image: res.data.data.display_url
+            }
+            const mealRes = await axiosSecure.post('/meal', mealItem);
+            if (mealRes.data.insertedId) {
+                reset();
+                Swal.fire({
+                    title: `${data.name} added!`,
+                    text: 'Item added successfully',
+                    icon: 'success',
+                    confirmButtonText: 'Close'
+                })
+            }
+        }
     };
 
     return (
