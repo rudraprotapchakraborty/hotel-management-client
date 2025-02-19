03ec09cb77c03612a35877945b4cbd57aafd200d
Rudra <rudra.phymos@gmail.com>
2025-02-19 19:25:18 +0530

    featured section updated

--- src/pages/Home/Featured.jsx
+++ src/pages/Home/Featured.jsx
@@ -1,5 +1,6 @@
 import SectionTitle from "../../components/SectionTitle";
-import featuredImg from "../../assets/home/featured.jpg";
+import featuredImg from "../../assets/meal/pizza-bg.jpg";
+import { NavLink } from "react-router-dom";
 
 const Featured = () => {
   return (
@@ -18,15 +19,15 @@ const Featured = () => {
 
         {/* Text Content */}
         <div className="md:ml-10 text-center md:text-left">
-          <p className="text-yellow-500 font-bold text-sm">March 20, 2023</p>
-          <h2 className="text-3xl font-semibold my-2">Where Can I Get Some?</h2>
+          <p className="text-yellow-500 font-bold text-sm">February 20, 2025</p>
+          <h2 className="text-3xl font-semibold my-2">Margherita Pizza</h2>
           <p className="text-lg mb-6 text-gray-200 dark:text-gray-300">
-            Experience the taste of our featured meal, prepared with love and the finest ingredients. A combination of freshness and flavor, this dish is perfect for any occasion.
+          Classic tomato sauce, mozzarella cheese, and fresh basil.
           </p>
 
-          <button className="btn bg-transparent text-white border-b-2 border-white hover:border-yellow-500 hover:text-yellow-500 transition-all duration-300 ease-in-out px-6 py-2 rounded-md dark:border-gray-400 dark:hover:border-yellow-500 dark:hover:text-yellow-500">
+          <NavLink to="/mealDetails/679c9908d05a74a71f7c03d7"><button className="btn bg-transparent text-white border-b-2 border-white hover:border-yellow-500 hover:text-yellow-500 transition-all duration-300 ease-in-out px-6 py-2 rounded-md dark:border-gray-400 dark:hover:border-yellow-500 dark:hover:text-yellow-500">
             Read More
-          </button>
+          </button></NavLink>
         </div>
       </div>
     </div>
